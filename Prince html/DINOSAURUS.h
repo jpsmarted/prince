7fa07c879e656bf4e8f035c8fe880e7e2478badf
jpsmarted <jayanta.code@gmail.com>
2019-08-18 12:12:26 +0530

    Add files via upload

---
+++ Prince html/DINOSAURUS.h
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+	<style type="text/css">
+		.rim1{
+			height: 200px;
+			width: 200px;
+			background-color:black ;
+		}
+		.rim2{
+			height: 200px;
+			width: 200px;
+			background-color: aqua;
+			margin-left: 200px;
+		}
+		.rim3{
+			height: 200PX;
+			width: 200PX;
+			background-color:yellow;
+			margin-left: 400px;
+		}
+	</style>
+</head>
+<body>
+<div class="rim1"></div>
+<div class="rim2"></div>
+<div class="rim3"></div>
+
+</body>
+</html>
